e624fe18e1afc297591b539a36ff734b95a0a481
Kamal <kamal.khalaf@businessboomers.net>
2019-10-15 12:43:31 +0200

    Initial commit

---
+++ mynewlibrary/src/main/java/com/khalaf/kamal/mynewlibrary/Validation.java
@@ -0,0 +1,17 @@
+package com.khalaf.kamal.mynewlibrary;
+/*
+ *
+ * Created by Kamal Khalaf on 10/15/2019.
+ * Contact : kamal.khalaf56@gmail.com
+ *
+ */
+
+public class Validation {
+
+
+    public boolean isEmpty(String s){
+
+        if (s.length() == 0 ) return true;
+        else return false;
+    }
+}
